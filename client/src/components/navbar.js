8e5adbded61ba574091ba98fdbe4ac69896fc866
Shubham Krishna <shubhamkrishnasound@gmail.com>
2022-06-07 22:10:29 +0530

    add navbar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React,{useState} from "react";
+import styled from "styled-components";
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
-
+import { NavbarContainer,
+    LeftContainer,
+    RightContainer,
+    NavbarExtendedContainer,
+    NavbarInnerContainer,
+    NavbarLinkContainer,
+    CustomLink,
+    Logo,
+    OpenLinksButton,
+    NavbarLinkExtended,} from "../styles/Navbar.style";
+    import LogoImg from "../styles/tenzify-logo.png"
 function Navbar() {
+    const [extendNavbar, setExtendNavbar] = useState(false);
     return (
         <div>
-            <ul>
+        <NavbarContainer>
+        <NavbarInnerContainer>
+        <LeftContainer>
+        <Logo src={LogoImg}></Logo>
+        </LeftContainer>
+        <RightContainer>
+        <NavbarLinkContainer>
                 <CustomLink to={"/"}>Home</CustomLink>
 
                 <CustomLink to={"/reserve-value"}>reserver value</CustomLink>
@@ -16,22 +34,44 @@ function Navbar() {
                 <CustomLink to={"/view-poll-results"}>
                     view poll result
                 </CustomLink>
-            </ul>
+                <OpenLinksButton
+              onClick={() => {
+                setExtendNavbar((curr) => !curr);
+              }}
+            >
+              {extendNavbar ? <>&#10005;</> : <> &#8801;</>}
+            </OpenLinksButton>
+                </NavbarLinkContainer>
+        </RightContainer>
+        </NavbarInnerContainer>
+        {extendNavbar && (
+        <NavbarExtendedContainer>
+          <NavbarLinkExtended to="/"> Home</NavbarLinkExtended>
+          <NavbarLinkExtended to={"/reserve-value"}>reserver value</NavbarLinkExtended>
+          <NavbarLinkExtended to={"/reserve-card"}>reserve card</NavbarLinkExtended>
+          <NavbarLinkExtended to={"/view-share-certificate"}>
+                    view share certificate</NavbarLinkExtended>
+          <NavbarLinkExtended  to={"/view-poll-results"}>
+                    view poll result</NavbarLinkExtended>
+        </NavbarExtendedContainer>
+        
+      )}
+            </NavbarContainer>
         </div>
     );
 }
 
-function CustomLink({ to, children, ...props }) {
-    const resolvedPath = useResolvedPath(to);
-    const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+// function CustomLink({ to, children, ...props }) {
+//     const resolvedPath = useResolvedPath(to);
+//     const isActive = useMatch({ path: resolvedPath.pathname, end: true });
 
-    return (
-        <li style={{ backgroundColor: `${isActive ? "black" : "blue"}` }}>
-            <Link to={to} {...props}>
-                {children}
-            </Link>
-        </li>
-    );
-}
+//     return (
+//         <li style={{ color: `${isActive ? "black" : "blue"}` }}>
+//             <Link to={to} {...props}>
+//                 {children}
+//             </Link>
+//         </li>
+//     );
+// }
 
 export default Navbar;
---
+++ client/src/pages/error/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Error() {
+    return <div>Opps! something went wrong</div>;
+}
+
+export default Error;
